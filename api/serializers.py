7e6654e12224880561bc4aee559d6f7aad06ec97
danylo-tiutiushkin-planeks <danylotiutiushkin@planeks.net>
2020-06-24 15:36:18 +0300

    added serializers

---
+++ api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+from .models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'link', 'author_name', 'amount_of_upvotes', 'creation_date')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'author_name', 'content', 'creation_date')
