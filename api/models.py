2d605349a94767de2893899603f37b59af4fa60a
danylo-tiutiushkin-planeks <danylotiutiushkin@planeks.net>
2020-06-24 15:31:08 +0300

    add basic models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Post:
+    title = models.CharField(max_length=100, blank=False, null=False)
+    link = models.URLField(blank=True, null=True)
+    author_name = models.CharField(max_length=100, blank=False, null=False)
+    amount_of_upvotes = models.IntegerField(default=0)
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
+class Comment:
+    content = models.TextField(null=False, blank=False)
+    author_name = models.CharField(max_length=100, blank=False, null=False)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, null=False, blank=False)
+    creation_date = models.DateTimeField(auto_now_add=True)
