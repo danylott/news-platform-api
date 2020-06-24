3fef294cf5ccbc9acabb62ec3e3d4226afadd501
danylo-tiutiushkin-planeks <danylotiutiushkin@planeks.net>
2020-06-24 16:10:17 +0300

    fix models.py - add inheritance from models.Model

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Post:
+class Post(models.Model):
     title = models.CharField(max_length=100, blank=False, null=False)
     link = models.URLField(blank=True, null=True)
     author_name = models.CharField(max_length=100, blank=False, null=False)
@@ -9,7 +9,7 @@ class Post:
     creation_date = models.DateTimeField(auto_now_add=True)
 
 
-class Comment:
+class Comment(models.Model):
     content = models.TextField(null=False, blank=False)
     author_name = models.CharField(max_length=100, blank=False, null=False)
     post = models.ForeignKey('Post', on_delete=models.CASCADE, null=False, blank=False)
